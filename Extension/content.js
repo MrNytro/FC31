da229b4bf032018c970569e4b3dd77a97d6062a9
Vishal Bajpai <117145234+MrNytro@users.noreply.github.com>
2023-05-18 15:48:39 +0530

    Finalized the form briefing

--- Extension/content.js
+++ Extension/content.js
@@ -1,59 +1,54 @@
 // Exclude the form if it contains search elements
 const forms = Array.from(document.querySelectorAll('form')).filter((form) => {
     const searchElements = Array.from(form.querySelectorAll('input')).filter((element) => {
-      return element.classList.contains('searchButton') || element.getAttribute('name') === 'search';
+        return element.classList.contains('searchButton') || element.getAttribute('name') == 'search';
     });
     return searchElements.length === 0;
-  });
-  
-  // Check the remaining forms
-  forms.forEach((form) => {
-    // Retrieve the form elements
+});
+
+// Check the remaining forms
+forms.forEach((form) => {
+    // Retrieve the form elements                                            STOP STALKING MY CODE
     const formElements = form.querySelectorAll('input');
-  
     // Identify the form type based on its CSS classes
     let formType = '';
     if (form.classList.contains('login-form')) {
-      formType = 'login';
+        formType = 'login';
     } else if (form.classList.contains('sign-up-form')) {
-      formType = 'sign-up';
+        formType = 'sign-up';
     } else if (form.classList.contains('registration-form')) {
-      formType = 'registration';
+        formType = 'registration';
     } else if (form.classList.contains('subscribe-form')) {
-      formType = 'subscription';
+        formType = 'subscription';
     } else {
-      formType = 'filling';
+        formType = 'filling';
     }
-  
     // Create the summary
     let summary = `This form is for ${formType} and requires the following information: `;
     const formFields = Array.from(formElements).map((element) => {
-      const fieldType = element.getAttribute('type');
-      let fieldLabel = '';
-      let exampleValue = '';
-  
-      if (fieldType === 'checkbox') {
-        // For checkboxes, include the name and state (checked/unchecked)
-        fieldLabel = element.getAttribute('name') + ': ' + (element.checked ? 'Checked' : 'Unchecked');
-      } else {
-        const placeholder = element.getAttribute('placeholder');
-        if (placeholder && !placeholder.includes('data:image')) {
-          // For regular input fields, use the placeholder value
-          fieldLabel = placeholder;
+        const fieldLabel = element.previousElementSibling?.textContent.trim();
+        const fieldType = element.getAttribute('type');
+        let exampleValue = '';
+        if (fieldType === 'checkbox') {
+            // For checkboxes, include the name and state (checked/unchecked)
+            return fieldLabel + ': ' + (element.checked ? 'Checked' : 'Unchecked');
         } else {
-          // For other types, fall back to a generic example value based on the field type
-          fieldLabel = element.getAttribute('name') || 'Example ' + fieldType;
+            if (fieldLabel && fieldLabel !== 'Example Hidden') {
+                // Use the text label as the field description
+                return fieldLabel;
+            } else if (element.getAttribute('placeholder') && element.getAttribute('placeholder') !== 'Example Hidden') {
+                // Use the placeholder text as the field description if available and not 'Example Hidden'
+                return element.getAttribute('placeholder');
+            } else {
+                // For other types, fall back to a generic example value based on the field type
+                return ' ';
+            }
         }
-      }
-  
-      return fieldLabel;
     });
-    summary += formFields.join(', ');
-  
-    // Create the summary element
+    summary += formFields.join(' ');
+    // Creating the summary element
     const summaryElement = document.createElement('p');
     summaryElement.textContent = summary;
-  
-    // Insert the summary element before the form
+    // Inserting the summary element before the form
     form.insertAdjacentElement('beforebegin', summaryElement);
-  });  
+});
